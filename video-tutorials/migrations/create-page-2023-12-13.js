58db31e03e7dd872525910e38657a2693c9d8015
sharofiddin <pardayev.sharofiddin@gmail.com>
2024-01-23 23:09:16 +0500

    forgotten changes

--- video-tutorials/migrations/create-page-2023-12-13.js
+++
@@ -1,6 +0,0 @@
-export function up(knex)
-  {     return knex.schema.createTable('pages', table => {
-    table.string('page_name').primary()
-    table.jsonb('page_data').defaultsTo('{}')
-  })}
-  export function down(knex) {   return knex.schema.dropTable('pages')}
